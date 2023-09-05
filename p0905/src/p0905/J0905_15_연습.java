878eb30f023686acf958dfdfa1cf8dfb90af2792
KOREAVC <KOREAVC@DESKTOP-PAVNKGG>
2023-09-05 20:08:59 +0900

    9월 5일 수업 남아서 연습

---
+++ p0905/src/p0905/J0905_15_연습.java
@@ -0,0 +1,113 @@
+package p0905;
+
+import java.util.Scanner;
+
+public class J0905_15_연습 {
+
+	public static void main(String[] args) {
+		//1-100 랜덤숫자를 생성해서 맞추는 프로그램을 구현하시오.
+//		Scanner sc = new Scanner(System.in);
+//		int[] num = new int[10];
+//		int random =(int)(Math.random()*100)+1;
+//		
+//		int i = 0;
+//		while(i<9) {
+//			System.out.println("숫자를 입력하세요.");
+//			int num1 = sc.nextInt();
+//			num[i] = num1;
+//				if(random==num1) {
+//					System.out.println("정답입니다.");
+//					break;
+//				}else if(random>=num1) {
+//					System.out.println("입력한 수보다 큰수입니다.");
+//				}else {
+//					System.out.println("입력한 수보다 작은수입니다.");
+//				}
+//			i++;
+//		}
+//		
+//		System.out.println("입력한번호 : ");
+//			for(int j=0;j<=i;j++) {
+//				System.out.printf("%d ",num[j]);
+//			}
+//		System.out.println();
+//		System.out.println("램덤번호 : "+random);
+		
+		//구구단
+		
+//		for(int i=2;i<=9;i++) {
+//		System.out.printf("[   %d단   ]\n",i);
+//		for(int j=1;j<=9;j++) {
+//			System.out.printf("%d * %d = %d \n",i,j,i*j);
+//		}
+//		System.out.println("-----------");
+//		}
+		
+//		for(int i=2;i<=9;i++) {
+//		System.out.printf("[   %d단   ] \n",i);
+//		for(int j=1;j<=9;j++) {
+//			System.out.println(i+" * "+j+" = "+(i*j));
+//			//System.out.printf("%d * %d = %d \n",i,j,i*j);
+//		}//for(2번)
+//		System.out.println("-----------");
+//	}//for
+
+//----------------------------------------------------------------
+				
+		//1-100 랜덤숫자를 생성해서 맞추는 프로그램을 구현하시오.
+//		Scanner sc = new Scanner(System.in);
+//		int[] num = new int[10];
+//		int random = (int)(Math.random()*100)+1;
+//		
+//		int i = 0;
+//		while(i<10) {
+//			System.out.println("숫자를 입력하세요.");
+//			int num1 = sc.nextInt();
+//			num[i] = num1;
+//				if(random==num1) {
+//					System.out.println("정답입니다.");
+//					break;
+//				}else if(random>num1) {
+//					System.out.println("입력한 수보다 큰수입니다.");
+//				}else {
+//					System.out.println("입력한 수보다 작은수입니다.");
+//				}
+//			i++;
+//		}
+//		System.out.println("입력한번호 : ");
+//		for(int j=0;j<=i;j++) {
+//			System.out.printf("%d ",num[j]);
+//		}
+//		System.out.println();
+//		System.out.println("램던번호 : "+random);
+		
+//----------------------------------------------------------------
+		
+//		Scanner sc = new Scanner(System.in);
+//		int[] num = new int[10];
+//		int random = (int)(Math.random()*100)+1;
+//		
+//		int i = 0;
+//		while(i<10) {
+//			System.out.println("숫자를 입력하세요.");
+//			int num1 = sc.nextInt();
+//			num[i] = num1;
+//			if(random==num1) {
+//				System.out.println("정답입니다.");
+//				break;
+//			}else if(random>num1) {
+//				System.out.println("입력한 수보다 큰수입니다.");
+//			}else {
+//				System.out.println("입력한 수보다 작은수입니다.");
+//			}
+//			i++;
+//		}
+//		System.out.println("입력한번호 : ");
+//		for(int j=0;j<i;j++) {
+//			System.out.printf("%d ",num[j]);
+//		}
+//		System.out.println();
+//		System.out.println("랜덤번호 : "+random);
+	}//main
+
+}//class
